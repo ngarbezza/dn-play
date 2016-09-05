503163ffbf925ff3e314e0091b1f01e3e60576ed
Nahuel Garbezza <n.garbezza@gmail.com>
2016-09-04 21:57:17 -0300

    mejorar el renderizado de las reglas

--- app/components/deduccion-body.js
+++ app/components/deduccion-body.js
@@ -1,14 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  cantidadDePasos: Ember.computed.alias('deduccion.pasos.length'),
+  formula: '',
+  motivo: 'premisa',
   actions: {
     agregarPaso() {
+      var motivo = this.get('motivo');
+      if (motivo === "regla") {
+        motivo = `${this.get('nombreRegla')} ${this.get('parametro1')}, ${this.get('parametro2')}, ${this.get('parametro3')}`;
+      }
       this.get('deduccion').pasos.pushObject({
         formula: this.get('formula'),
-        motivo: this.get('motivo')
+        motivo: motivo
       });
       this.set('formula', '');
-      this.set('motivo', '');
     }
   }
 });
