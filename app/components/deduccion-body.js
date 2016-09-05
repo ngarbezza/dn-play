5c87c06f9f8f865674b093b3038bb7cca012fd17
Nahuel Garbezza <n.garbezza@gmail.com>
2016-09-04 22:30:11 -0300

    editar el resultado esperado, y transformar not and y ors

--- app/components/deduccion-body.js
+++ app/components/deduccion-body.js
@@ -1,17 +1,31 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  cantidadDePasos: Ember.computed.alias('deduccion.pasos.length'),
+  editandoResultado: true,
+  pasos: [],
+  cantidadDePasos: Ember.computed.alias('pasos.length'),
   formula: '',
   motivo: 'premisa',
   actions: {
+    editarResultado() {
+      this.toggleProperty('editandoResultado');
+    },
+    actualizarResultado() {
+      this.toggleProperty('editandoResultado');
+      this.set('resultadoEsperado', this.get('resultadoEsperado').replace(/not/g, '¬')
+        .replace(/or/g, '⋁')
+        .replace(/and/g, '⋀'));
+    },
     agregarPaso() {
       var motivo = this.get('motivo');
-      if (motivo === "regla") {
+      if (motivo === 'regla') {
         motivo = `${this.get('nombreRegla')} ${this.get('parametro1')}, ${this.get('parametro2')}, ${this.get('parametro3')}`;
       }
-      this.get('deduccion').pasos.pushObject({
-        formula: this.get('formula'),
+      this.get('pasos').pushObject({
+        formula: this.get('formula')
+          .replace(/not/g, '¬')
+          .replace(/or/g, '⋁')
+          .replace(/and/g, '⋀'),
         motivo: motivo
       });
       this.set('formula', '');
