074992f7191cf8eec445789dfe197d80941026b8
Nahuel Garbezza <n.garbezza@gmail.com>
2016-09-05 00:26:21 -0300

    imprimir reglas

--- app/components/deduccion-body.js
+++ app/components/deduccion-body.js
@@ -1,27 +1,47 @@
 import Ember from 'ember';
 import { formularizar } from 'dn-play/helpers/formularizar';
 
+// TODO: mover todo esto a helpers/clases para poder testearlo
+const REGLAS = [
+  { id: 'ed', nombre: 'E ⋁', parametros: 3 },
+  { id: 'id', nombre: 'I ⋁', parametros: 1 },
+  { id: 'ec', nombre: 'E ⋀', parametros: 1 },
+  { id: 'ic', nombre: 'I ⋀', parametros: 2 },
+  { id: 'en', nombre: 'E ¬', parametros: 2 },
+  { id: 'in', nombre: 'I ¬', parametros: 0 },
+  { id: 'ei', nombre: 'E →', parametros: 2 },
+  { id: 'ii', nombre: 'I →', parametros: 0 },
+  { id: 'efsq', nombre: 'EFSQ', parametros: 0 }
+];
+
+var reglaConNombre = function (id) {
+  return REGLAS.findBy('id', id);
+};
+
+var imprimirRegla = function (id, parametro1, parametro2, parametro3) {
+  var regla = reglaConNombre(id);
+  var resultado = `${regla.nombre} `;
+  var parametros = reglaConNombre(id).parametros;
+  if (parametros > 0) { resultado += parametro1; }
+  if (parametros > 1) { resultado += `, ${parametro2}`; }
+  if (parametros > 2) { resultado += `, ${parametro3}`; }
+  return resultado;
+};
+
 export default Ember.Component.extend({
   editandoResultado: true,
   pasos: [],
   cantidadDePasos: Ember.computed.alias('pasos.length'),
   formula: '',
   motivo: 'premisa',
-  reglas: [
-    { id: 'ed', nombre: 'E ⋁' },
-    { id: 'id', nombre: 'I ⋁' },
-    { id: 'ec', nombre: 'E ⋀' },
-    { id: 'ic', nombre: 'I ⋀' },
-    { id: 'en', nombre: 'E ¬' },
-    { id: 'in', nombre: 'I ¬' },
-    { id: 'ei', nombre: 'E →' },
-    { id: 'ii', nombre: 'I →' },
-    { id: 'efsq', nombre: 'EFSQ' }
-  ],
+  reglas: REGLAS,
   nombreRegla: 'ed',
   parametro1: '1',
   parametro2: '2',
   parametro3: '3',
+  cantidadDeParametros: Ember.computed('nombreRegla', function () {
+    return reglaConNombre(this.get('nombreRegla')).parametros;
+  }),
   actions: {
     editarResultado() {
       this.toggleProperty('editandoResultado');
@@ -33,7 +53,12 @@ export default Ember.Component.extend({
     agregarPaso() {
       var motivo = this.get('motivo');
       if (motivo === 'regla') {
-        motivo = `${this.get('nombreRegla')} ${this.get('parametro1')}, ${this.get('parametro2')}, ${this.get('parametro3')}`;
+        motivo = imprimirRegla(
+          this.get('nombreRegla'),
+          this.get('parametro1'),
+          this.get('parametro2'),
+          this.get('parametro3')
+        );
       }
       this.get('pasos').pushObject({
         formula: formularizar(this.get('formula')),
